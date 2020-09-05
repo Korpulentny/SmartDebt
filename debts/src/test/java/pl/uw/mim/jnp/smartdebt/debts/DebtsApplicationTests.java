080f8aa0fc3d3a6251407bc06c280de058301960
Tomasz Patyna <patynatomek@gmail.com>
2020-09-05 19:09:41 +0200

    initial debts commit

---
+++ debts/src/test/java/pl/uw/mim/jnp/smartdebt/debts/DebtsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uw.mim.jnp.smartdebt.debts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
