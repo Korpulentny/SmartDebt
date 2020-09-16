ec1d5b6acb2efc21d93473b2d939ddea4005f870
Tomasz Patyna <patynatomek@gmail.com>
2020-09-16 08:37:33 +0200

    to localhost

--- debts/src/main/java/pl/uw/mim/jnp/smartdebt/debts/models/constants/Constants.java
+++ debts/src/main/java/pl/uw/mim/jnp/smartdebt/debts/models/constants/Constants.java
@@ -2,8 +2,8 @@ package pl.uw.mim.jnp.smartdebt.debts.models.constants;
 
 public class Constants {
 	public static final String AUTHORIZATION_HEADER_STRING = "Authorization";
-	public static final String USER_SERVICE_URL = "http://users:1327/";
-	public static final String USER_SERVICE_AUTHORIZATION_URL = "http://users:1327/authorize";
-	public static final String USER_SERVICE_USER_EXISTS_URL = "http://users:1327/user-exists";
+	public static final String USER_SERVICE_URL = "http://localhost:1327/";
+	public static final String USER_SERVICE_AUTHORIZATION_URL = "http://localhost:1327/authorize";
+	public static final String USER_SERVICE_USER_EXISTS_URL = "http://localhost:1327/user-exists";
 	public static final String UNAUTHORIZED_REQUEST_MESSAGE = "Brak dostępu";
 }
--- front/src/services/authentication.service.js
+++ front/src/services/authentication.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_SERVICE_URL = "http://users:1327/";
+const USER_SERVICE_URL = "users:1327/";
 
 class AuthenticationService {
 	jwtHeader() {
